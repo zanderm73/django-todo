07de91d42daa46de44d897295e13cbdaf7205935
Ubuntu <ubuntu@ip-172-31-88-196.ec2.internal>
2019-08-26 20:25:44 +0000

    initial commit

---
+++ todo/test_forms.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from .forms import ItemForm
+
+# Create your tests here.
+class TestToDoItemForm(TestCase):
+    
+    def test_can_create_an_item_with_just_a_name(self):
+        form = ItemForm({'name': 'Create Tets'})
+        self.assertTrue(form.is_valid())
+        
+    def test_correct_message_for_missing_name(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
+        
