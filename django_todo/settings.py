51ce227dfc5619527948beebc9fe87b3993b2f4c
Ubuntu <ubuntu@ip-172-31-88-196.ec2.internal>
2019-08-27 06:57:39 +0000

    prepping requirments and settings file for deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
+DATABASES = {'default': dj_database_url.parse("postgres://sgbemghdsjffqa:e707e4c99760891833e7ef55d75da1999be694b216079bdef4f29fc23c0347f1@ec2-54-246-121-32.eu-west-1.compute.amazonaws.com:5432/dblvjvv0jrv9ps")}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
